cdde7eaf096564933c2ca433ef238c7dc6a03427
STIkhiynyy <korobka.bohdan@yandex.ru>
2025-09-01 18:20:07 +0300

    add file

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { getRandomValues } = require('crypto');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 6060,
+  }
+};
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: './style/main.css'
+    })
+  ],
+    module:{
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg)$/i,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                  HtmlWebpackPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                  HtmlWebpackPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+   },
+  ...devServer(develop)
+});
