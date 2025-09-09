469c3b389c6152fb85e2243d21b0e6f148fe8f6f
STIkhiynyy <korobka.bohdan@yandex.ru>
2025-09-09 14:20:08 +0300

    работа над стилями2

--- webpack.config.js
+++ webpack.config.js
@@ -48,14 +48,6 @@ module.exports = ({develop}) => ({
                   MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
                 ]
             },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
         ]
    },
   ...devServer(develop)
